12ab175f0a19310ae9f244d0fee407ce499ec487
Robbo00 <rscrog606@west-mec.org>
2025-08-06 15:29:50 -0700

    home almost done

--- Home/index.js
+++ Home/index.js
@@ -6,14 +6,36 @@ let swish = document.getElementById('dy')
 let point = document.getElementById('point')
 let vis = document.getElementById('vis')
 let cont = document.getElementById('cont')
+let left = document.getElementById('lef')
+let right = document.getElementById('righ')
+
+let switchBacks = ['/background/dogs.mp4', '/background/two.mp4']
+let switchHeads = ['Activities Page', 'Gallery Page']
+let switchSub = ['Visit this page to explore different varities of summer activities', 'Visit this page to see beautiful pictures']
+
 
 function intro(){
     change.style.top = '-50px'
-    change.style.display = 'none'
+    setTimeout(function(){change.style.display = 'none'}, 1000) 
     fresh.style.top = '0'
     setTimeout(function(){cont.style.visibility = 'visible'},  1000) 
     main.setAttribute('src', '/background/dogs.mp4')
     setTimeout(function(){swish.style.width = '100%'}, 1000) 
-    setTimeout(function(){cont.style.top = '40%'}, 2500) 
-    setTimeout(function(){vis.style.visibility = 'visible'}, 3300) 
+    setTimeout(function(){cont.style.top = '36%'}, 2000) 
+    setTimeout(function(){vis.style.visibility = 'visible'}, 2700) 
+    setTimeout(function(){
+        left.classList.add('a'); 
+        right.classList.add('a')}, 3000) 
+}
+function back(){
+        cont.style.visibility = 'hidden'
+
+    change.style.top = '0px'
+    change.style.display = 'initial'
+    fresh.style.top = '100%'
+    swish.style.width = '0'
+    cont.style.top = '-100%'
+    vis.style.visibility = 'hidden'
+    left.classList.remove('a'); 
+    right.classList.remove('a')
 }
