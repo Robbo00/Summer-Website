026a40d34d9c2481cdb17371bfa3041b5eb6270d
Robbo00 <rscrog606@west-mec.org>
2025-08-06 07:38:06 -0700

    home almost done

--- Home/index.js
+++ Home/index.js
@@ -2,10 +2,18 @@ let background = document.getElementById('back')
 let change = document.getElementById('first')
 let fresh = document.getElementById('real')
 let main = document.getElementById('mainBack')
+let swish = document.getElementById('dy')
+let point = document.getElementById('point')
+let vis = document.getElementById('vis')
+let cont = document.getElementById('cont')
 
 function intro(){
     change.style.top = '-50px'
     change.style.display = 'none'
     fresh.style.top = '0'
+    setTimeout(function(){cont.style.visibility = 'visible'},  1000) 
     main.setAttribute('src', '/background/dogs.mp4')
+    setTimeout(function(){swish.style.width = '100%'}, 1000) 
+    setTimeout(function(){cont.style.top = '40%'}, 2500) 
+    setTimeout(function(){vis.style.visibility = 'visible'}, 3300) 
 }
